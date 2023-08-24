e49cebe0e50d734ffeaeb6f99dc386b4c58add8d
Codelab-Solux <codelabsolux@gmail.com>
2023-08-24 11:43:51 +0000

    LOG_LEVEL removal

--- afridemia/settings.py
+++ afridemia/settings.py
@@ -186,26 +186,26 @@ AUTH_USER_MODEL = "accounts.CustomUser"  # !!! very important !!!
 LOGGING_CONFIG = None
 
 # Get loglevel from env
-LOGLEVEL = env('DJANGO_LOGLEVEL', 'info').upper()
+# LOGLEVEL = env('DJANGO_LOGLEVEL', 'info').upper()
 
-logging.config.dictConfig({
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'console': {
-            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s',
-        },
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'console',
-        },
-    },
-    'loggers': {
-        '': {
-            'level': LOGLEVEL,
-            'handlers': ['console',],
-        },
-    },
-})
+# logging.config.dictConfig({
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'console': {
+#             'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s',
+#         },
+#     },
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'console',
+#         },
+#     },
+#     'loggers': {
+#         '': {
+#             'level': LOGLEVEL,
+#             'handlers': ['console',],
+#         },
+#     },
+# })
