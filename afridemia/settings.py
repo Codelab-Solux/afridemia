5bc922a93dcb0047a38fd11d900f24ffd3958c8a
Codelab-Solux <codelabsolux@gmail.com>
2023-09-13 17:05:50 +0000

    secret key patch

--- afridemia/settings.py
+++ afridemia/settings.py
@@ -24,7 +24,8 @@ environ.Env.read_env(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-cnp+n3wdx_8*77$rewd8)wwi99j)+0f2q892qu&u)y7t%#rvdw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
