7bf1676e037ea810666acb307f0a85cb6bf4fedd
Codelab-Solux <codelabsolux@gmail.com>
2023-09-16 12:05:02 +0000

    csrf patch

--- afridemia/settings.py
+++ afridemia/settings.py
@@ -30,11 +30,11 @@ SECRET_KEY = 'django-insecure-cnp+n3wdx_8*77$rewd8)wwi99j)+0f2q892qu&u)y7t%#rvdw
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # DEBUG = eval(env('DEBUG'))
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '62.72.19.182','http://*', 'https://*',
                  'afridemia.com', 'www.afridemia.com', 'afridemia.onrender.com', 'www.afridemia.onrender.com']
-CSRF_TRUSTED_ORIGINS = ['https://afridemia.onrender.com/*']
+CSRF_TRUSTED_ORIGINS = ['127.0.0.1','https://afridemia.onrender.com/*']
 
 # Application definition
 
