cacb004a7d3b70281e0f5ccbd6b063657c003931
Codelab-Solux <codelabsolux@gmail.com>
2023-09-16 11:05:59 +0000

    db switched

--- afridemia/settings.py
+++ afridemia/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-cnp+n3wdx_8*77$rewd8)wwi99j)+0f2q892qu&u)y7t%#rvdw
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # DEBUG = eval(env('DEBUG'))
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '62.72.19.182',
                  'afridemia.com', 'www.afridemia.com', 'afridemia.onrender.com', 'www.afridemia.onrender.com']
@@ -95,17 +95,17 @@ WSGI_APPLICATION = 'afridemia.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': dj_database_url.parse('postgres://afridemia_user:QoPxMPYISB6KGsSWkZU2oSH3fkTbrPB7@dpg-ck0t4r36fquc73amh510-a.frankfurt-postgres.render.com/afridemia')
+    'default': dj_database_url.parse('postgres://afridemia_user:QoPxMPYISB6KGsSWkZU2oSH3fkTbrPB7@dpg-ck0t4r36fquc73amh510-a.frankfurt-postgres.render.com/afridemia')
 
-    'default': {
-        'ENGINE': env('DATABASE_ENGINE'),
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USERNAME'),
-        'PASSWORD': env('DATABASE_PASSWORD'),
-        'HOST': env('DATABASE_HOST'),
-        'PORT': env('DATABASE_PORT'),
+    # 'default': {
+    #     'ENGINE': env('DATABASE_ENGINE'),
+    #     'NAME': env('DATABASE_NAME'),
+    #     'USER': env('DATABASE_USERNAME'),
+    #     'PASSWORD': env('DATABASE_PASSWORD'),
+    #     'HOST': env('DATABASE_HOST'),
+    #     'PORT': env('DATABASE_PORT'),
 
-    }
+    # }
 }
 
 
