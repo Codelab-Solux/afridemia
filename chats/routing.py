85b97dbbee81b38ae7ada6e9c48e7af57a123a43
Codelab-Solux <codelabsolux@gmail.com>
2023-09-18 23:34:30 +0000

    cache amended

--- chats/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import consumers
-
-websocket_urlpatterns = [
-    path('chats/<str:id>/', consumers.PrivateChatConsumer.as_asgi()),
-    path('notify/', consumers.ChatNotificationConsumer.as_asgi()),
-]
